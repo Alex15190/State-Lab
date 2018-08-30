d99aa7c2a6ce2cc48a4a4f2737af89091c2c374c
Alex Chekodanov <checkodanov@gmail.com>
2018-08-30 15:22:53 +0300

    Initial Commit

---
+++ State Lab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  State Lab
+//
+//  Created by Alex Chekodanov on 30.08.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ State Lab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  State Lab
+//
+//  Created by Alex Chekodanov on 30.08.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
