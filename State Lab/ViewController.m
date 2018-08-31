ba981b280a63cc413c6e1b52f84cae409806f522
Alex Chekodanov <checkodanov@gmail.com>
2018-08-31 11:19:13 +0300

    Initial commit

--- State Lab/ViewController.m
+++ State Lab/ViewController.m
@@ -10,15 +10,127 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UILabel *label;
+@property (strong, nonatomic) UIImage *smiley;
+@property (strong, nonatomic) UIImageView *smileyView;
+@property (strong, nonatomic) UISegmentedControl *segmentedControl;
+
 @end
 
 @implementation ViewController
+{
+    BOOL animate;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGRect bounds = self.view.bounds;
+    CGRect labelFrame = CGRectMake(bounds.origin.x, CGRectGetMidY(bounds) - 50, bounds.size.width, 100);
+    self.label = [[UILabel alloc] initWithFrame:labelFrame];
+    self.label.font = [UIFont fontWithName:@"Helvetica" size:70];
+    self.label.text = @"Bazinga!";
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.backgroundColor = [UIColor clearColor];
+    
+    CGRect smileyFrame = CGRectMake(CGRectGetMidX(bounds)- 42, CGRectGetMidY(bounds)/2 - 42, 84, 84);
+    self.smileyView = [[UIImageView alloc] initWithFrame:smileyFrame];
+    self.smileyView.contentMode = UIViewContentModeCenter;
+    NSString *smileyPath = [[NSBundle mainBundle] pathForResource:@"smiley" ofType:@"png"];
+    self.smiley = [UIImage imageWithContentsOfFile:smileyPath];
+    self.smileyView.image = self.smiley;
+    
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"One", @"Two", @"Three", @"Four", nil]];
+    self.segmentedControl.frame = CGRectMake(bounds.origin.x + 20, 50, bounds.size.width - 40, 30);
+    
+    [self.view addSubview:self.segmentedControl];
+    [self.view addSubview:self.smileyView];
+    [self.view addSubview:self.label];
+    
+    NSNumber *indexNumber = [[NSUserDefaults standardUserDefaults] objectForKey:@"selectedIndex"];
+    if (indexNumber)
+    {
+        NSInteger selectedIndex = [indexNumber intValue];
+        self.segmentedControl.selectedSegmentIndex = selectedIndex;
+    }
+    
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self selector:@selector(applicationWillResignActive) name:UIApplicationWillResignActiveNotification object:nil];
+    [center addObserver:self selector:@selector(applicationDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+    
+    [center addObserver:self selector:@selector(applicationDidEnterBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [center addObserver:self selector:@selector(applicationWillEnterForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
 }
 
+- (void)applicationDidEnterBackground
+{
+    NSLog(@"VC: %@", NSStringFromSelector(_cmd));
+    UIApplication *app = [UIApplication sharedApplication];
+    
+    __block UIBackgroundTaskIdentifier taskId;
+    taskId = [app beginBackgroundTaskWithExpirationHandler:^{
+        NSLog(@"Background task ran out of time and was terminated.");
+        [app endBackgroundTask:taskId];
+    }];
+    
+    if (taskId == UIBackgroundTaskInvalid)
+    {
+        NSLog(@"Failed to start background task!");
+        return;
+    }
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"Starting background task with %f seconds remaining", app.backgroundTimeRemaining);
+        self.smiley = nil;
+        self.smileyView.image = nil;
+        NSInteger selectedIndex = self.segmentedControl.selectedSegmentIndex;
+        [[NSUserDefaults standardUserDefaults] setInteger:selectedIndex forKey:@"selectedIndex"];
+        
+        [NSThread sleepForTimeInterval:25];
+        NSLog(@"Finishing background task with %f seconds remaining", app.backgroundTimeRemaining);
+        [app endBackgroundTask:taskId];
+    });
+}
+
+- (void)applicationWillEnterForeground
+{
+    NSLog(@"VC: %@", NSStringFromSelector(_cmd));
+    NSString *smileyPath = [[NSBundle mainBundle] pathForResource:@"smiley" ofType:@"png"];
+    self.smiley = [UIImage imageWithContentsOfFile:smileyPath];
+    self.smileyView.image = self.smiley;
+}
+
+- (void)applicationWillResignActive
+{
+    NSLog(@"VC: %@", NSStringFromSelector(_cmd));
+    animate = NO;
+}
+
+- (void)applicationDidBecomeActive
+{
+    NSLog(@"VC: %@", NSStringFromSelector(_cmd));
+    animate = YES;
+    [self rotateLabelDown];
+}
+
+- (void)rotateLabelDown
+{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.label.transform =CGAffineTransformMakeRotation(M_PI);
+    } completion:^(BOOL finished) {
+        [self rotateLabelUp];
+    }];
+}
+
+- (void)rotateLabelUp
+{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.label.transform =CGAffineTransformMakeRotation(0);
+    } completion:^(BOOL finished) {
+        if (self->animate)
+            [self rotateLabelDown];
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
